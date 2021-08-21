e612be16b71950e74cb233085e35c141a6bbdbc2
_MoonLight_ <moonlight8978@gmail.com>
2021-08-21 17:18:41 +0700

    Remove console

--- contracts/TokenSwapping.sol
+++ contracts/TokenSwapping.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "hardhat/console.sol";
 
 contract TokenSwapping is Ownable {
     struct Rate {
