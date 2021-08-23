b26dd9a7854ee5ebd4e7de4641236041508c69db
_MoonLight_ <moonlight8978@gmail.com>
2021-08-23 17:13:17 +0700

    Prepare deploy script

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,9 +2,35 @@ import "@typechain/hardhat";
 import "@nomiclabs/hardhat-ethers";
 import "@nomiclabs/hardhat-waffle";
 import { HardhatUserConfig } from "hardhat/types";
+import * as fs from "fs";
+import * as path from "path";
+import { task } from "hardhat/config";
+import mnemonic from "./secret.json";
+
+task("accounts", "Prints the list of accounts", async () => {
+  const { ethers } = await import("hardhat");
+  const accounts = await ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
 
 const config: HardhatUserConfig = {
   solidity: "0.8.4",
+  networks: {
+    hardhat: {
+      accounts: {
+        mnemonic: fs.readFileSync(path.join(__dirname, "secret")).toString(),
+      },
+    },
+    "bsc-testnet": {
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545",
+      chainId: 97,
+      gasPrice: 20000000000,
+      accounts: [mnemonic.privateKey],
+    },
+  },
 };
 
 export default config;
---
+++ scripts/deploy.ts
@@ -0,0 +1,21 @@
+import { ethers } from "hardhat";
+
+async function main() {
+  const [deployer] = await ethers.getSigners();
+
+  console.log("Deploying contracts with the account:", deployer.address);
+
+  console.log("Account balance:", (await deployer.getBalance()).toString());
+
+  const TokenSwapping = await ethers.getContractFactory("TokenSwapping");
+  const contract = await TokenSwapping.deploy();
+
+  console.log("Contract address:", contract.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
