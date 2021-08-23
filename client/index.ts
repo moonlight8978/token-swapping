ab8e5d511df3258ffe742f49f0737ab06981d999
_MoonLight_ <moonlight8978@gmail.com>
2021-08-23 19:15:51 +0700

    Fix scripts to deploy to local network

--- client/index.ts
+++ client/index.ts
@@ -1,6 +1,9 @@
 import { ethers } from "ethers";
-import accounts from "./wallets/accounts.json";
+import secret from "../secret.json";
 import TokenSwapping from "../artifacts/contracts/TokenSwapping.sol/TokenSwapping.json";
+import { TokenSwapping as ITokenSwapping } from "./../typechain";
+
+const { accounts } = secret;
 
 const provider = new ethers.providers.JsonRpcProvider({
   url: "http://127.0.0.1:8545",
@@ -8,11 +11,19 @@ const provider = new ethers.providers.JsonRpcProvider({
 
 const run = async () => {
   const balance = await provider.getBalance(accounts[0].address);
+  console.log(balance);
   const contract = new ethers.Contract(
     "0x1E02890AC1a68C60C5aEBF8Af852DC0dE28D5954",
-    TokenSwapping.abi
+    TokenSwapping.abi,
+    provider
+  ) as ITokenSwapping;
+  console.log(await contract.owner());
+  console.log(
+    await contract.getRate(
+      ethers.constants.AddressZero,
+      ethers.constants.AddressZero
+    )
   );
-  contract.connect(provider);
 };
 
 run();
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,10 +2,10 @@ import "@typechain/hardhat";
 import "@nomiclabs/hardhat-ethers";
 import "@nomiclabs/hardhat-waffle";
 import { HardhatUserConfig } from "hardhat/types";
-import * as fs from "fs";
-import * as path from "path";
 import { task } from "hardhat/config";
-import mnemonic from "./secret.json";
+import secret from "./secret.json";
+
+const { accounts } = secret;
 
 task("accounts", "Prints the list of accounts", async () => {
   const { ethers } = await import("hardhat");
@@ -19,16 +19,11 @@ task("accounts", "Prints the list of accounts", async () => {
 const config: HardhatUserConfig = {
   solidity: "0.8.4",
   networks: {
-    hardhat: {
-      accounts: {
-        mnemonic: fs.readFileSync(path.join(__dirname, "secret")).toString(),
-      },
-    },
     "bsc-testnet": {
       url: "https://data-seed-prebsc-1-s1.binance.org:8545",
       chainId: 97,
       gasPrice: 20000000000,
-      accounts: [mnemonic.privateKey],
+      accounts: [accounts[2].key],
     },
   },
 };
